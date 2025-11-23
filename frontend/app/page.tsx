700067b8c840d1d9d026486e26d850f7e686a127
matt <yuuki@superyuuki.com>
2025-11-22 19:15:16 -0500

    we do not use ssr (lol)

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,8 @@
-import Dashboard from "./dashboard";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const Dashboard = dynamic(() => import("./dashboard"), { ssr: false });
 
 export default function Page() {
   return <Dashboard />;
