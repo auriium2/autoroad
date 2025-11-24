8403f250f56ab2678bc046c7a3ec72d6b0a29312
matt <yuuki@superyuuki.com>
2025-11-24 04:47:34 -0500

    remove bad scripts and terrible tests

--- backend/clear_cache_and_test.py
+++
@@ -1,8 +0,0 @@
-"""Clear cache and test if requirements work."""
-
-from api.services.cache import clear_cache
-
-print("[CACHE] Clearing all caches...")
-clear_cache()
-print("[CACHE] Cache cleared!")
-print("[CACHE] Please restart the backend server to use fresh data.")
