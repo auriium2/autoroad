62d92c18b63d9f8468ac4c9da9cf2a42569052e8
matt <yuuki@superyuuki.com>
2026-01-29 18:58:10 -0500

    more intelligence proofing

--- frontend/src/components/tutorial/steps.ts
+++ frontend/src/components/tutorial/steps.ts
@@ -136,7 +136,7 @@ export const tutorialSteps: StepOptions[] = [
   {
     id: 'welcome',
     title: 'Welcome to Autoroad',
-    text: `Autoroad helps you complete your degree by picking the easiest classes that satisfies it.</br></br>Let's learn how to use it! Most of the interesting stuff is towards the end of the tutorial, so please be patient :)`,
+    text: `Autoroad helps you complete your degree by picking the easiest classes that satisfies it.</br></br>Let's learn how to use it! <strong>It's highly recommended you complete the tutorial to prevent simple errors!</strong> Autoroad has a learning curve beyond Courseroad.`,
     buttons: [
       { text: 'Skip', action: function() { restoreState(); return this.complete(); }, secondary: true },
       { text: 'Start', action: function() { return this.next(); } },
